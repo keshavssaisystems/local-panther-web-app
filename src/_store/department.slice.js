1a5624c33d87728dcf335e7828a6e90f88719283
Tirupati Rao <trao@saisoftek.net>
2023-09-11 17:53:17 +0530

    TFS-8084

--- src/_store/department.slice.js
+++ src/_store/department.slice.js
@@ -21,7 +21,7 @@ function createInitialState() {
 }
 
 function createExtraActions() {
-  const baseUrl = `${process.env.REACT_APP_MASTER_API_URL}/api`;
+  const baseUrl = `${process.env.REACT_APP_JOB_API_URL}/api`;
 
   return {
     getDepartment: getDepartment(),
