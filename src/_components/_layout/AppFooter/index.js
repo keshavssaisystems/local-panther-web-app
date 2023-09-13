58f0990df995fa75d53c8479985dedcda01c8392
Saisystemspramod <pramod.turakane@businesswire.com>
2023-09-13 13:22:07 +0530

    Updated sidebar header and footer css

--- src/_components/_layout/AppFooter/index.js
+++ src/_components/_layout/AppFooter/index.js
@@ -1,19 +1,12 @@
 import React from "react";
-// import MegaMenuFooter from "./Components/FooterMegaMenu";
-// import FooterDots from "./Components/FooterDots";
+import "./appfooter.scss";
 
 export function AppFooter() {
   return (
     <div className="app-footer fixed-footer">
       <div className="app-footer__inner">
-        <div className="app-footer-left">
-          {/* <FooterDots /> */}
-          {/* panther left */}
-        </div>
-        <div className="app-footer-right">
-          {/* <MegaMenuFooter /> */}
-          {/* panther right */}
-        </div>
+        <div className="app-footer-left"></div>
+        <div className="app-footer-right"></div>
       </div>
     </div>
   );
--- src/_components/_layout/AppSidebar/index.jsx
+++ src/_components/_layout/AppSidebar/index.jsx
@@ -1,25 +1,22 @@
 /* eslint-disable react/display-name, jsx-a11y/click-events-have-key-events */
+import React, { useEffect, useState } from "react";
 import { Navigation } from "react-minimal-side-navigation";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-
-import Icon from "awesome-react-icons";
-import React, { useEffect, useState } from "react";
-
+import "./appsidebar.scss";
 import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
 
 export const AppSidebar = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const menuDtoList = useSelector(x => x?.auth?.menuList);
+  const menuDtoList = useSelector((x) => x?.auth?.menuList);
 
-
-  const [menuItems, setMenuItems] = useState([])
+  const [menuItems, setMenuItems] = useState([]);
 
   useEffect(() => {
     if (menuDtoList) {
-      let data = []
+      let data = [];
       for (let variable of menuDtoList) {
         let obj = {
           itemId: variable.path,
@@ -33,13 +30,12 @@ export const AppSidebar = () => {
           isadd: variable.isadd,
           isedit: variable.isedit,
           isdelete: variable.isdelete,
-          subMenuList: variable.subMenuList
-        }
-        data.push(obj)
+          subMenuList: variable.subMenuList,
+        };
+        data.push(obj);
       }
-      setMenuItems(data)
+      setMenuItems(data);
     }
-
   }, []);
 
   return (
@@ -47,12 +43,14 @@ export const AppSidebar = () => {
       {/* Sidebar Overlay */}
       <div
         onClick={() => setIsSidebarOpen(false)}
-        className={`fixed inset-0 z-20 block transition-opacity bg-black opacity-50 lg:hidden ${isSidebarOpen ? "block" : "hidden"
-          }`}
+        className={`appsidebar fixed inset-0 z-20 block transition-opacity bg-black opacity-50 lg:hidden ${
+          isSidebarOpen ? "block" : "hidden"
+        }`}
       />
       <div
-        className={`fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 ease-out transform translate-x-0 bg-white border-r-2 lg:translate-x-0 lg:static lg:inset-0 ${isSidebarOpen ? "ease-out translate-x-0" : "ease-in -translate-x-full"
-          }`}
+        className={`appsidebar fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 ease-out transform translate-x-0 bg-white border-r-2 lg:translate-x-0 lg:static lg:inset-0 ${
+          isSidebarOpen ? "ease-out translate-x-0" : "ease-in -translate-x-full"
+        }`}
       >
         <Navigation
           activeItemId={location.pathname}
@@ -60,7 +58,6 @@ export const AppSidebar = () => {
             navigate(itemId);
           }}
           items={menuItems}
-       
         />
       </div>
     </>
