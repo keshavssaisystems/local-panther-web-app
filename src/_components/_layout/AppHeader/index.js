104b00952329cb29db3541a7a04e084945c32aff
Saisystemspramod <pramod.turakane@businesswire.com>
2023-09-13 14:00:36 +0530

    Updated header logo

--- src/_components/_layout/AppHeader/index.js
+++ src/_components/_layout/AppHeader/index.js
@@ -7,33 +7,36 @@ import { UserBox } from "./Components/UserBox";
 import logo from "../../../assets/utils/images/panther-logo.png";
 
 export function AppHeader({
-  headerBackgroundColor = 'white',
+  headerBackgroundColor = "white",
   enableMobileMenuSmall = true,
-  enableHeaderShadow = true
-  }) {
-    return (
-      <>
-        <CSSTransitionGroup component="div"
-          className={cx("app-header", headerBackgroundColor, {
-            "header-shadow": enableHeaderShadow,
+  enableHeaderShadow = true,
+}) {
+  return (
+    <>
+      <CSSTransitionGroup
+        component="div"
+        className={cx("app-header", headerBackgroundColor, {
+          "header-shadow": enableHeaderShadow,
+        })}
+        transitionname="HeaderAnimation"
+        transitionAppear={true}
+        transitionAppearTimeout={1500}
+        transitionEnter={false}
+        transitionLeave={false}
+      >
+        <div
+          className={cx("app-header__content", {
+            "header-mobile-open": enableMobileMenuSmall,
           })}
-          transitionname="HeaderAnimation" transitionAppear={true}
-          transitionAppearTimeout={1500} transitionEnter={false} transitionLeave={false}>
-
-          <div
-            className={cx("app-header__content", {
-              "header-mobile-open": enableMobileMenuSmall,
-            })}>
-            <div className="app-header-left">
-              <img src={logo} width={"130px"} alt="logo"/>
-            </div>
-            <div className="app-header-right">
-              <UserBox />
-            </div>
+        >
+          <div className="app-header-left">
+            <img src={logo} width={"130px"} height={"48px"} alt="logo" />
           </div>
-        </CSSTransitionGroup>
-      </>
-    );
+          <div className="app-header-right">
+            <UserBox />
+          </div>
+        </div>
+      </CSSTransitionGroup>
+    </>
+  );
 }
-
-
