f19176df2a07569863aee407a1064862f85266df
sbRaavi <sbraavi@tech.saisystems.com>
2023-09-18 17:18:40 +0530

    TFS-8287 reverted merge conflict in app js file

--- src/_containers/app/App.jsx
+++ src/_containers/app/App.jsx
@@ -43,7 +43,7 @@ export function App() {
                   </PrivateRoute>
                 }
               />
-              <Route path="/JobList" element={<JobList />} />
+              <Route path="/job-list" element={<JobList />} />
               <Route path="/JobDetail" element={<JobDetail />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/create-job" element={<CreateJob />} />
