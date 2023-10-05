7eea4a612605a3b7b994a03f52e91d6c0dfde077
ngpanther <ngouda@saisystems.com>
2023-10-05 21:42:55 +0530

    class to function

--- src/_containers/admin/AdminDashboardDetails.js
+++ src/_containers/admin/AdminDashboardDetails.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import avatar1 from "assets/utils/images/avatars/1.jpg";
 import avatar2 from "assets/utils/images/avatars/2.jpg";
@@ -30,117 +30,20 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default class AdminDashboardDetails extends Component {
-  constructor(props) {
-    super(props);
+const AdminDashboardDetails = () => {
+  const [visible, setVisible] = useState(true)
 
-    this.togglePop1 = this.togglePop1.bind(this);
-
-    this.state = {
-      visible: true,
-      popoverOpen1: false,
-
-      optionsRadial: {
-        chart: {
-          height: 350,
-          type: "radialBar",
-          toolbar: {
-            show: true,
-          },
-        },
-        plotOptions: {
-          radialBar: {
-            startAngle: -135,
-            endAngle: 225,
-            hollow: {
-              margin: 0,
-              size: "70%",
-              background: "#fff",
-              image: undefined,
-              imageOffsetX: 0,
-              imageOffsetY: 0,
-              position: "front",
-              dropShadow: {
-                enabled: true,
-                top: 3,
-                left: 0,
-                blur: 4,
-                opacity: 0.24,
-              },
-            },
-            track: {
-              background: "#fff",
-              strokeWidth: "67%",
-              margin: 0, // margin is in pixels
-              dropShadow: {
-                enabled: true,
-                top: -3,
-                left: 0,
-                blur: 4,
-                opacity: 0.35,
-              },
-            },
-
-            dataLabels: {
-              showOn: "always",
-              name: {
-                offsetY: -10,
-                show: true,
-                color: "#888",
-                fontSize: "17px",
-              },
-              value: {
-                formatter: function (val) {
-                  return parseInt(val);
-                },
-                color: "#111",
-                fontSize: "36px",
-                show: true,
-              },
-            },
-          },
-        },
-        fill: {
-          type: "gradient",
-          gradient: {
-            shade: "dark",
-            type: "horizontal",
-            shadeIntensity: 0.5,
-            gradientToColors: ["#ABE5A1"],
-            inverseColors: true,
-            opacityFrom: 1,
-            opacityTo: 1,
-            stops: [0, 100],
-          },
-        },
-        stroke: {
-          lineCap: "round",
-        },
-        labels: ["Percent"],
-      },
-      seriesRadial: [76],
-    };
-    this.onDismiss = this.onDismiss.bind(this);
+  const onDismiss = () => {
+    setVisible( value => !value);
   }
 
-  togglePop1() {
-    this.setState({
-      popoverOpen1: !this.state.popoverOpen1,
-    });
-  }
-
-  onDismiss() {
-    this.setState({ visible: false });
-  }
-
-  render() {
     return (
       <Fragment>
         <TransitionGroup>
           <CSSTransition component="div" classNames="TabsAnimation" appear={true}
             timeout={1500} enter={false} exit={false}>
             <div className="adminDashboardDetails"> 
-              <Alert className="mbg-3" color="info" isOpen={this.state.visible} toggle={this.onDismiss}>
+              <Alert className="mbg-3" color="info" isOpen={visible} toggle={onDismiss}>
                 <span className="pe-2">
                   <FontAwesomeIcon icon={faQuestionCircle} />
                 </span>
@@ -638,5 +541,6 @@ export default class AdminDashboardDetails extends Component {
         </TransitionGroup>
       </Fragment>
     );
-  }
 }
+
+export default AdminDashboardDetails;
