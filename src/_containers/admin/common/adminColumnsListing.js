daeb1523206f4a93760d5386fa1f66917651afb8
ngpanther <ngouda@saisystems.com>
2023-10-15 07:59:00 +0530

    user roles menumapping

--- src/_containers/admin/common/adminColumnsListing.js
+++ src/_containers/admin/common/adminColumnsListing.js
@@ -133,7 +133,6 @@ buttonsList: [
     ]
 }
 
-
 export const users = {
   title: 'Users',
   icon: "assets/utils/images/candidate.svg",
@@ -141,14 +140,14 @@ export const users = {
   columns : [
   {
     name: "User role",
-    id: "role",
-    selector: row => row.role,
+    id: "rolename",
+    selector: row => row.rolename,
     sortable: true,
   },
   {
     name: "First name",
     id: "firstName",
-    selector: row => row.firstName,
+    selector: row => row.firstname,
     sortable: true,
   },
   {
@@ -157,35 +156,28 @@ export const users = {
     sortable: true,
   },
   {
-    name: "Address",
-    selector: row => row.address,
-    sortable: true,
-  },
-  {
-    name: "City",
-    selector: row => row.city,
-    sortable: true,
-  },
-  {
-    name: "State",
-    selector: row => row.state,
-    sortable: true,
-  },
-  {
-    name: "Zip code",
-    selector: row => row.zipCode,
+    name: "User name",
+    selector: row => row.username,
     sortable: true,
   },
   {
     name: "Email",
+    id: "email",
     selector: row => row.email,
     sortable: true,
   },
   {
-    name: "Mobile",
-    selector: row => row.mobile,
+    name: "Phone",
+    id: "phonenumber",
+    selector: row => row.phonenumber,
     sortable: true,
   },
+  {
+    name: "Status",
+    id: "isactive",
+    selector: row => row.isactive,
+    sortable: true,
+  }
   ],
     searchFilter: [
       {
@@ -216,8 +208,8 @@ export const roles = {
   columns : [
   {
     name: "Role",
-    id: "role",
-    selector: row => row.role,
+    id: "rolename",
+    selector: row => row.rolename,
     sortable: true,
   },
   {
@@ -227,35 +219,42 @@ export const roles = {
     sortable: true,
   },
   {
+    name: "Status",
+    id: "isactive",
+    selector: row => row.isactive,
+    sortable: true,
+  },
+  {
     name: "# of Users",
+    id: "numOfUsers",
     selector: row => row.numOfUsers,
     sortable: true,
-  }
+    }
   ],
   searchFilter: [
-      {
-        name: "Role",
-        id: "role",
-      },
     {
-      name: "#ofUsers",
-      id: "numOfUsers",
+      name: "Role",
+      id: "role",
+    },
+    {
+      name: "Status",
+      id: "isactive",
     },
     ],
   buttonsList: [
-      {
-        name: "Search",
-        id: "search",
-      },
-      {
-        name: "Add",
-        id: "add",
-        selector: row => row.add,
-        sortable: false,
-        align : "right"
-      },
-    ]
-  };
+    {
+      name: "Search",
+      id: "search",
+    },
+    {
+      name: "Add",
+      id: "add",
+      selector: row => row.add,
+      sortable: false,
+      align : "right"
+    },
+  ]
+};
 
 export const menuMapping = {
   title: 'Menu Mapping',
