ff9417ccae340920e50fb8433b6b927af183a053
ngpanther <ngouda@saisystems.com>
2023-10-15 06:04:08 +0530

    updates for compnay add

--- src/_containers/admin/common/adminListing.js
+++ src/_containers/admin/common/adminListing.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import companyLogo from "assets/utils/images/candidate.svg";
 import { customers, company, users, roles, menuMapping } from "_containers/admin/common/adminColumnsListing"
 import PageTitle from "_components/common/pagetitle";
-import { Row, Col, Card, CardBody, CardHeader, Button, FormGroup, InputGroup, Input } from "reactstrap";
+import { Row, Col, Card, CardBody, CardHeader, Button, FormGroup, InputGroup, Input, Modal, ModalHeader, ModalBody } from "reactstrap";
 import "_containers/admin/common/adminListing.scss"
 import DataTable from "react-data-table-component";
 import { useDispatch, useSelector } from "react-redux";
 import { getCompanies, getCustomers } from '_containers/admin/_redux/adminListing.slice'
 import { useEffect } from "react";
+import { NewCompany } from "_components/common/newcompany";
+import errorIcon from "assets/utils/images/error_icon.png";
+import successIcon from "assets/utils/images/success_icon.svg";
 
 
-export const AdminListing = ({entity}) => {
+export const AdminListing = ({ entity }) => {
   const dispatch = useDispatch()
   const {
     data,
     loading = false } = useSelector((state) => state?.adminListing ?? {});
 
   let title, icon, listingTitle, columns = [], searchFilter = [], buttonsList = [];
+
+  const [error, setError] = useState(false)
+  const [message, setMessage] = useState("")
+  const [success, setSuccess] = useState(false)
+  const [addComp, setAddComp] = useState(false)
+  const [editMode, setEditMode] = useState(false)
+  const [selectedRowData, setSelectedRowData] = useState(null);
+  const [editingData, setEditingData] = useState(null);
+
+  const [newComp, setNewComp] = useState({
+    newCompName: { value: "", error: false },
+    newIndusName: { value: "", error: false },
+
+    newCompDesc: { value: "" },
+    newCompEmp: { value: "" },
+    newCompAdd: { value: "" },
+    newCompState: { value: 0 },
+    newCompCity: { value: 0 },
+    newCompCountry: { value: 0 },
+    newCompLog: { value: 0 },
+    newCompZip: { value: "" },
+    newCompEmail: { value: "" },
+    newCompPhonenum: { value: "" },
+  });
 
   switch (entity) {
     case "customers":
@@ -89,8 +116,60 @@ export const AdminListing = ({entity}) => {
     }
   };
 
-  const cardFilters = (searchFilter).map( item => (
-    < Col lg = "3" md = "3" sm = "12" sx = "12" >
+  const handleRowClick = (row) => {
+    setSelectedRowData(row);
+    setEditMode(true);
+    setAddComp(true); // Open the modal
+  };
+
+  const onAddClick = () => {
+    setEditMode(false); // Reset edit mode
+    setAddComp(true); // Open the modal
+  };
+
+  const close = () => {
+    setAddComp(false)
+    setSuccess(false)
+    setEditMode(false); // Reset edit mode
+    setEditingData(null); // Reset editing data
+  }
+
+
+
+  const onUpdateNewComp = (evt) => {
+    let newData = {};
+    newData[evt.target.name].value = evt.target.value;
+  };
+
+  const onSaveClick = async () => {
+    let newCompData = {};
+
+
+      if (newCompData.newCompName.value === "") {
+        newCompData.newCompName.error = true;
+      }
+      if (newCompData.newIndusName.value === "") {
+        newCompData.newIndusName.error = true;
+      }
+      if (newCompData.newCompAdd.value === "") {
+        newCompData.newCompAdd.error = true;
+      }
+      if (newCompData.newCompCity.value === "") {
+        newCompData.newCompCity.error = true;
+      }
+      if (newCompData.newCompCountry.value === "") {
+        newCompData.newCompCountry.error = true;
+      }
+    if (newCompData.newCompName.error || newCompData.newIndusName.error || newCompData.newCompEmail.error || newCompData.newCompEmp.error || newCompData.newCompPhonenum.error
+      || newCompData.newCompAdd.error || newCompData.newCompState.error || newCompData.newCompState.error || newCompData.newCompCountry.error || newCompData.newCompZip.error || newCompData.newCompDesc.error) {
+
+      return;
+    }
+
+  };
+
+  const cardFilters = (searchFilter).map(item => (
+    < Col lg="3" md="3" sm="12" sx="12" >
       <Input name={item.id} type="select">
         <option value="">{item.name}</option>
       </Input>
@@ -102,7 +181,7 @@ export const AdminListing = ({entity}) => {
       <FormGroup>
         <InputGroup>
           <div className="admin-list btn-actions-pane-right ">
-            <Button className="mb-2 me-2  " color="primary">
+            <Button className="mb-2 me-2  " color="primary" onClick={onAddClick}>
               {item.name}
             </Button>
           </div>
@@ -136,11 +215,80 @@ export const AdminListing = ({entity}) => {
                 fixedHeader
                 fixedHeaderScrollHeight="400px"
                 customStyles={customStyles}
+                onRowClicked={handleRowClick}
               />
             </CardBody>
           </Card>
         </Col>
       </Row>
+      {
+        success ? <div>
+          <Modal isOpen={success}>
+            <Card>
+              <CardBody>
+                <div className="d-flex justify-content-center mb-3 mt-4">
+                  <img src={successIcon} alt="success-icon" />
+                </div>
+                <div className="mb-0 d-flex justify-content-center popup-message">
+                  {message}
+                </div>
+
+                <div className="margin-custom">
+                  <Row>
+                    <Col className="d-flex justify-content-center mb-4 ">
+                      <Button className="" onClick={() => close()}>
+                        OK
+                      </Button>
+                    </Col>
+                  </Row>
+                </div>
+              </CardBody>
+            </Card>
+          </Modal>
+        </div> : <></>
+      }
+      <Modal isOpen={error}>
+        <Card>
+          <CardBody>
+            <div className="d-flex justify-content-center mb-3 mt-4">
+              <img src={errorIcon} alt="success-icon" />
+            </div>
+            <div className="mb-0 d-flex justify-content-center popup-message">
+              {message}
+            </div>
+
+            <div className="margin-custom">
+              <Row>
+                <Col className="d-flex justify-content-center mb-4 ">
+                  <Button className="" onClick={() => setError(false)}>
+                    OK
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          </CardBody>
+        </Card>
+      </Modal>
+      {addComp ? <div>
+        <Modal isOpen={addComp}>
+          <ModalHeader toggle={() => close()} charCode="Y">
+            <strong className="card-title-text">
+              {editMode ? "Edit Company" : "Add New Company"}
+            </strong>
+          </ModalHeader>
+
+          <ModalBody>
+
+            {/* <NewCompany data={newComp} updateVal={(e) => onUpdateNewComp(e)} editingData={editingData} editMode={editMode} /> */}
+            <NewCompany editingData={editingData} editMode={editMode} />
+
+            <Button color="primary" onClick={() => onSaveClick()}>
+              Submit
+            </Button>
+
+          </ModalBody>
+        </Modal>
+      </div> : <></>}
     </>
   );
 }
