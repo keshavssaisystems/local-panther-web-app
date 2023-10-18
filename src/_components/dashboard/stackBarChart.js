7449dcea3433f1c9a3810c8d8a4b0b9dfc72c0b3
Tirupati Rao <trao@saisoftek.net>
2023-10-18 14:06:37 +0530

    TFS-8631

--- src/_components/dashboard/stackBarChart.js
+++ src/_components/dashboard/stackBarChart.js
@@ -3,7 +3,6 @@ import { Card, CardBody } from "reactstrap";
 import Chart from "react-apexcharts";
 
 export function StackBarChart({ graphData }) {
-  console.log(graphData);
   const stackValues = [
     "Matched candidates",
     "Liked by customer",
@@ -26,59 +25,26 @@ export function StackBarChart({ graphData }) {
     "Rejected by customer": "rejectedbycustomer",
     "Rejected by candidate": "rejectedbycandidate",
   };
-  let JobTitle = [];
-  let mainArray = [];
-  let matchedcandidate = [];
-  let likedbycustomer = [];
-  let likedbycandidate = [];
-  let likedbyboth = [];
-  let acceptedbycustomer = [];
-  let acceptedbycandidate = [];
-  let acceptedbyboth = [];
-  let rejectedbycustomer = [];
-  let rejectedbycandidate = [];
+  let mainArray = {
+    matchedcandidate: [],
+    likedbycustomer: [],
+    likedbycandidate: [],
+    likedbyboth: [],
+    acceptedbycustomer: [],
+    acceptedbycandidate: [],
+    rejectedbycustomer: [],
+    rejectedbycandidate: [],
+    acceptedbyboth: [],
+    jobtitle: [],
+    jobid: [],
+  };
   let seriesDataArray = [];
   if (graphData?.length !== undefined) {
     graphData.forEach((jobValue) => {
-      if (jobValue.hasOwnProperty("matchedcandidate")) {
-        matchedcandidate.push(jobValue["matchedcandidate"]);
-      }
-      if (jobValue.hasOwnProperty("likedbycustomer")) {
-        likedbycustomer.push(jobValue["likedbycustomer"]);
-      }
-      if (jobValue.hasOwnProperty("likedbycandidate")) {
-        likedbycandidate.push(jobValue["likedbycandidate"]);
-      }
-      if (jobValue.hasOwnProperty("likedbyboth")) {
-        likedbyboth.push(jobValue["likedbyboth"]);
-      }
-      if (jobValue.hasOwnProperty("acceptedbycustomer")) {
-        acceptedbycustomer.push(jobValue["acceptedbycustomer"]);
-      }
-      if (jobValue.hasOwnProperty("acceptedbycandidate")) {
-        acceptedbycandidate.push(jobValue["acceptedbycandidate"]);
-      }
-      if (jobValue.hasOwnProperty("acceptedbyboth")) {
-        acceptedbyboth.push(jobValue["acceptedbyboth"]);
-      }
-      if (jobValue.hasOwnProperty("rejectedbycustomer")) {
-        rejectedbycustomer.push(jobValue["rejectedbycustomer"]);
-      }
-      if (jobValue.hasOwnProperty("rejectedbycandidate")) {
-        rejectedbycandidate.push(jobValue["rejectedbycandidate"]);
-      }
-      if (jobValue.hasOwnProperty("jobtitle")) {
-        JobTitle.push(jobValue["jobtitle"]);
-      }
-      mainArray["matchedcandidate"] = matchedcandidate;
-      mainArray["likedbycustomer"] = likedbycustomer;
-      mainArray["likedbycandidate"] = likedbycandidate;
-      mainArray["likedbyboth"] = likedbyboth;
-      mainArray["acceptedbycustomer"] = acceptedbycustomer;
-      mainArray["acceptedbycandidate"] = acceptedbycandidate;
-      mainArray["rejectedbycustomer"] = rejectedbycustomer;
-      mainArray["rejectedbycandidate"] = rejectedbycandidate;
-      mainArray["acceptedbyboth"] = acceptedbyboth;
+      Object.keys(jobValue).forEach((key) => {
+        console.log(jobValue[key]);
+        mainArray[key].push(jobValue[key]);
+      });
     });
     stackValues.forEach((element) => {
       let seriesData = {
@@ -89,6 +55,18 @@ export function StackBarChart({ graphData }) {
     });
     console.log(seriesDataArray);
   }
+  var colorPalette = [
+    "#00D8B6",
+    "#008FFB",
+    "#FEB019",
+    "#FF4560",
+    "#775DD0",
+    "#01BFD6",
+    "#5564BE",
+    "#F7A600",
+    "#EDCD24",
+    "#F74F58",
+  ];
   let baroptions = {
     chart: {
       type: "bar",
@@ -105,8 +83,20 @@ export function StackBarChart({ graphData }) {
         columnWidth: "30%",
       },
     },
+    colors: [
+      "#008FFB",
+      "#00D8B6",
+      "#FEB019",
+      "#FF4560",
+      "#775DD0",
+      "#01BFD6",
+      "#5564BE",
+      "#F7A600",
+      "#EDCD24",
+      "#F74F58",
+    ],
     series: seriesDataArray,
-    labels: JobTitle,
+    labels: mainArray["jobtitle"],
     xaxis: {
       labels: {
         show: true,
