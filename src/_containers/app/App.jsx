723eee7239f7be6e10fa3d00021b0ba21f834219
Tirupati Rao <trao@saisoftek.net>
2023-10-19 17:32:30 +0530

    POC

--- src/_containers/app/App.jsx
+++ src/_containers/app/App.jsx
@@ -4,7 +4,7 @@ import { history } from "_helpers";
 import { PrivateRoute } from "_components";
 import {
   AdminDashboard,
-  // CustomerDashboard,
+  CustomerDashboard,
   CandidateDashboard,
 } from "_containers/dashboard/Dashboard";
 
@@ -54,7 +54,9 @@ import { CustomerReportCandidateStatus } from "_containers/customer/reports/cust
 import { CustomerVideoScreen } from "../../firebase/customerVideo";
 import { CandVideoScreen } from "../../firebase/candvideo";
 import { AdminListing } from "_containers/admin/common/adminListing";
-import CustomerDashboard from "_containers/customer/dashboard/customerDashboard";
+import { CustomerChat } from "../../firebase/chat/customer";
+import { CandidateChat } from "firebase/chat/candidate";
+// import CustomerDashboard from "_containers/customer/dashboard/customerDashboard";
 
 export function App() {
   const authUser = useSelector((state) => state.auth.token);
@@ -303,6 +305,14 @@ export function App() {
               </PrivateRoute>
             }
           />
+          <Route
+            path="/customer-chat"
+            element={
+              <PrivateRoute>
+                <CustomerChat />
+              </PrivateRoute>
+            }
+          />
         </>
       );
     } else {
@@ -357,6 +367,14 @@ export function App() {
             element={
               <PrivateRoute>
                 <CandVideoScreen />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/candidate-chat"
+            element={
+              <PrivateRoute>
+                <CandidateChat />
               </PrivateRoute>
             }
           />
---
+++ src/firebase/chat/candidate.js
@@ -0,0 +1,70 @@
+import firebase from "firebase/app";
+import React, { useRef, useState } from "react";
+import "firebase/firestore";
+import { firebaseConfig } from "../index";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { Alert, Row, Col } from "reactstrap";
+import "./chat.scss";
+const userId = localStorage.getItem("userId");
+
+export function CandidateChat() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  const firestore = firebase.firestore();
+  const dummy = useRef();
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      sender: userId,
+    });
+
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="say something nice"
+        />
+
+        <button type="submit" disabled={!formValue}>
+          {">"}
+        </button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  //   console.log(props);
+  const { text, sender } = props.message;
+  const messageClass = sender === userId ? "sent" : "received";
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
