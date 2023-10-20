43ac8865510f29a57fe2d644fa7e36952465f6e9
Vinit Pal <vinitpal@saisoftek.net>
2023-10-20 17:39:38 +0530

    Feature: disabled tooltip for calender

--- src/_widgets/reactBigCalender/ReactBigCalender.js
+++ src/_widgets/reactBigCalender/ReactBigCalender.js
@@ -45,6 +45,7 @@ export function ReactBigCalender() {
           }}
           today={true}
           views={{ month: true }}
+          toolbar={false}
           onSelectEvent={handleSelectEvent}
         />
       </CardBody>
