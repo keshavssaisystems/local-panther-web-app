ed035dc30ee758bd9a3c9b5e8cdfeedd64b3bf29
sbRaavi <sbraavi@tech.saisystems.com>
2023-11-02 19:25:45 +0530

    TFS-8803 Candidate Bug Fixes

--- src/_containers/candidate/educationalInfo.js
+++ src/_containers/candidate/educationalInfo.js
@@ -325,7 +325,7 @@ export function CandidateEducation(props) {
             </div>
             <div className="mb-3 d-flex justify-content-center rejected-success-text">
               {" "}
-              want to delete the Qualification!!
+              want to delete the Education details!!
             </div>
             <div>
               <Row>
