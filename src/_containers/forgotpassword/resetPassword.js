d04d6fe456bcff4c2076245b4f3731cdd0d0a748
sbRaavi <sbraavi@tech.saisystems.com>
2023-11-02 17:43:58 +0530

    TFS-8803 deployement changes

--- src/_containers/forgotpassword/resetPassword.js
+++ src/_containers/forgotpassword/resetPassword.js
@@ -56,9 +56,9 @@ export function ResetPassword() {
                 <div className="h-100 d-flex justify-content-center align-items-center bg-plum-plate">
                   <div
                     className="slide-img-bg"
-                    // style={{
-                    //   backgroundImage: "url(" + loginBgImg + ")",
-                    // }}
+                    style={{
+                      backgroundImage: "url(" + loginBgImg + ")",
+                    }}
                   />
                   <div className="login-slider-title">
                     <p>Experts In Human Capital</p>
