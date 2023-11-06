6ae80727bcbf723d0db0f57e4cf83b5afaa49bf3
sbRaavi <sbraavi@tech.saisystems.com>
2023-11-06 12:14:33 +0530

    TFS-8803 deployment issue changes

--- src/_containers/candidate/resumeDetails.js
+++ src/_containers/candidate/resumeDetails.js
@@ -75,7 +75,7 @@ export function ResumeDetails(props) {
       form.append("Candidateresumeid", resumeDetails.candidateresumeid);
       form.append(
         "Candidateid",
-        JSON.parse(localStorage.getItem("userDetails")).InternalUserId
+        JSON.parse(localStorage.getItem("userDetails"))?.InternalUserId
       );
       form.append("Resumepath", "");
       form.append("Resumefile", acceptedFiles[0]);
@@ -105,7 +105,7 @@ export function ResumeDetails(props) {
       const form = new FormData();
       form.append(
         "Candidateid",
-        JSON.parse(localStorage.getItem("userDetails")).InternalUserId
+        JSON.parse(localStorage.getItem("userDetails"))?.InternalUserId
       );
       form.append("Resumefile", acceptedFiles[0]);
 
