09b9243f66fea592ca349944400a11b98ed103a2
sbRaavi <sbraavi@tech.saisystems.com>
2023-11-08 16:49:06 +0530

    TFS-8803 video and env issue changes

--- src/_containers/candidate/dashboard/dashboardCounts.js
+++ src/_containers/candidate/dashboard/dashboardCounts.js
@@ -1,130 +1,206 @@
 import React, { useState } from "react";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Card } from "reactstrap";
 import { useSelector } from "react-redux";
 import { history } from "_helpers";
+import { WidgetCard } from "_components/dashboard/widgetCard";
 
 export function DashboardCounts() {
   const counts = useSelector(
     (state) => state.candidateDashboard.dashboardCounts
   );
   const navigateToJobsPage = function (e, count, tab) {
-    if (count == 0) {
+    if (count === 0) {
       e.preventDefault();
       return;
     } else {
       history.navigate("/job-list-" + tab);
     }
   };
+
   return (
     <>
       <Row>
-        <Col md="6" xl="3">
-          <div className="card mb-3 widget-content bg-night-fade">
-            <div className="widget-content-wrapper text-white">
-              <div className="widget-content-left">
-                <div className="widget-heading">Matched jobs</div>
-              </div>
-              <div className="widget-content-right">
-                <div className="widget-numbers text-white">
-                  <span
-                    onClick={(e) =>
-                      navigateToJobsPage(e, counts.matchedcandidate, "matched")
-                    }
-                    style={{
-                      cursor:
-                        counts.matchedcandidate == 0 ? "block" : "pointer",
-                    }}
-                  >
-                    {counts ? counts.matchedcandidate : 0}
-                  </span>
-                </div>
-              </div>
+        <Col>
+          <Card
+            className={
+              "widget-chart widget-chart2 text-start mb-3 card-btm-border card-shadow-" +
+              "alternate border-alternate"
+            }
+          >
+            <div className="widget-chat-wrapper-outer">
+              <Row>
+                <Col md="4">
+                  <div className="icon-wrapper rounded-circle mt-1">
+                    <div className={"icon-wrapper-bg bg-alternate"} />
+                    <i className={"lnr-graduation-hat text-alternate"} />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="widget-chart-content">
+                    <div className="widget-title opacity-5 ">Matched jobs</div>
+                    <div className="widget-numbers mt-2 fsize-4 mb-0 w-100">
+                      <div className="widget-chart-flex align-items-center">
+                        <div
+                          onClick={(e) =>
+                            navigateToJobsPage(
+                              e,
+                              counts.matchedcandidate,
+                              "matched"
+                            )
+                          }
+                          style={{
+                            cursor:
+                              counts.matchedcandidate === 0
+                                ? "block"
+                                : "pointer",
+                          }}
+                        >
+                          {counts ? counts.matchedcandidate : 0}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
             </div>
-          </div>
+          </Card>
         </Col>
-        <Col md="6" xl="3">
-          <div className="card mb-3 widget-content bg-arielle-smile">
-            <div className="widget-content-wrapper text-white">
-              <div className="widget-content-left">
-                <div className="widget-heading">Interviews scheduled</div>
-              </div>
-              <div className="widget-content-right">
-                <div className="widget-numbers text-white">
-                  <span
-                    onClick={(e) =>
-                      navigateToJobsPage(
-                        e,
-                        counts.interveiwSchedule,
-                        "interview"
-                      )
-                    }
-                    style={{
-                      cursor:
-                        counts.interveiwSchedule == 0 ? "block" : "pointer",
-                    }}
-                  >
-                    {counts ? counts.interveiwSchedule : 0}
-                  </span>
-                </div>
-              </div>
+        <Col>
+          <Card
+            className={
+              "widget-chart widget-chart2 text-start mb-3 card-btm-border card-shadow-" +
+              "primary border-primary"
+            }
+          >
+            <div className="widget-chat-wrapper-outer">
+              <Row>
+                <Col md="4">
+                  <div className="icon-wrapper rounded-circle mt-1">
+                    <div className={"icon-wrapper-bg bg-primary"} />
+                    <i className={"lnr-calendar-full text-primary"} />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="widget-chart-content">
+                    <div className="widget-title opacity-5 ">
+                      Interviews scheduled
+                    </div>
+                    <div className="widget-numbers mt-2 fsize-4 mb-0 w-100">
+                      <div className="widget-chart-flex align-items-center">
+                        <div
+                          onClick={(e) =>
+                            navigateToJobsPage(
+                              e,
+                              counts.interveiwSchedule,
+                              "interview"
+                            )
+                          }
+                          style={{
+                            cursor:
+                              counts.interveiwSchedule === 0
+                                ? "block"
+                                : "pointer",
+                          }}
+                        >
+                          {counts ? counts.interveiwSchedule : 0}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
             </div>
-          </div>
+          </Card>
         </Col>
-        <Col md="6" xl="3">
-          <div className="card mb-3 widget-content bg-happy-green">
-            <div className="widget-content-wrapper text-white">
-              <div className="widget-content-left">
-                <div className="widget-heading">Accepted</div>
-              </div>
-              <div className="widget-content-right">
-                <div className="widget-numbers text-white">
-                  <span
-                    onClick={(e) =>
-                      navigateToJobsPage(
-                        e,
-                        counts.acceptedbycandidate,
-                        "accepted"
-                      )
-                    }
-                    style={{
-                      cursor:
-                        counts.acceptedbycandidate == 0 ? "block" : "pointer",
-                    }}
-                  >
-                    {counts ? counts.acceptedbycandidate : 0}
-                  </span>
-                </div>
-              </div>
+        <Col>
+          <Card
+            className={
+              "widget-chart widget-chart2 text-start mb-3 card-btm-border card-shadow-" +
+              "success border-success"
+            }
+          >
+            <div className="widget-chat-wrapper-outer">
+              <Row>
+                <Col md="4">
+                  <div className="icon-wrapper rounded-circle mt-1">
+                    <div className={"icon-wrapper-bg bg-success"} />
+                    <i className={"lnr-thumbs-up text-success"} />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="widget-chart-content">
+                    <div className="widget-title opacity-5 ">Accepted</div>
+                    <div className="widget-numbers mt-2 fsize-4 mb-0 w-100">
+                      <div className="widget-chart-flex align-items-center">
+                        <div
+                          onClick={(e) =>
+                            navigateToJobsPage(
+                              e,
+                              counts.acceptedbycandidate,
+                              "accepted"
+                            )
+                          }
+                          style={{
+                            cursor:
+                              counts.acceptedbycandidate === 0
+                                ? "block"
+                                : "pointer",
+                          }}
+                        >
+                          {counts ? counts.acceptedbycandidate : 0}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
             </div>
-          </div>
+          </Card>
         </Col>
-        <Col md="6" xl="3">
-          <div className="card mb-3 widget-content bg-strong-bliss">
-            <div className="widget-content-wrapper text-white">
-              <div className="widget-content-left">
-                <div className="widget-heading">Rejected</div>
-              </div>
-              <div className="widget-content-right">
-                <div className="widget-numbers text-white">
-                  <span
-                    onClick={(e) =>
-                      navigateToJobsPage(
-                        e,
-                        counts.acceptedbycandidate,
-                        "rejected"
-                      )
-                    }
-                    style={{
-                      cursor:
-                        counts.acceptedbycandidate == 0 ? "block" : "pointer",
-                    }}
-                  >
-                    {counts ? counts.acceptedbycandidate : 0}
-                  </span>
-                </div>
-              </div>
+        <Col>
+          <Card
+            className={
+              "widget-chart widget-chart2 text-start mb-3 card-btm-border card-shadow-" +
+              "danger border-danger"
+            }
+          >
+            <div className="widget-chat-wrapper-outer">
+              <Row>
+                <Col md="4">
+                  <div className="icon-wrapper rounded-circle mt-1">
+                    <div className={"icon-wrapper-bg bg-danger"} />
+                    <i className={"lnr-user text-danger"} />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="widget-chart-content">
+                    <div className="widget-title opacity-5 ">Rejected</div>
+                    <div className="widget-numbers mt-2 fsize-4 mb-0 w-100">
+                      <div className="widget-chart-flex align-items-center">
+                        <div
+                          onClick={(e) =>
+                            navigateToJobsPage(
+                              e,
+                              counts.rejectedbycandidate,
+                              "rejected"
+                            )
+                          }
+                          style={{
+                            cursor:
+                              counts.rejectedbycandidate === 0
+                                ? "block"
+                                : "pointer",
+                          }}
+                        >
+                          {counts ? counts.rejectedbycandidate : 0}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
             </div>
-          </div>
+          </Card>
         </Col>
       </Row>
     </>
