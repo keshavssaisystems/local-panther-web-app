a712f2ffb3e3a561f2ce07cb89136c907a5845d3
Tirupati Rao <trao@saisoftek.net>
2023-11-14 16:06:27 +0530

    TFS-9161

--- src/_components/scheduleInterview/videoInterviewDetails.js
+++ src/_components/scheduleInterview/videoInterviewDetails.js
@@ -245,7 +245,7 @@ export function VideoInterviewDetails({
             ? "Scheduled"
             : interviewDetail?.isrejected === true
             ? "Rejected by candidate"
-            : "no response from candidate"}
+            : "No response from candidate"}
         </p>
       </div>
       {showInviteCard === true && (
