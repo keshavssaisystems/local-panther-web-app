c9a2d29cef015307b09ee360132a1039b83831e5
Tirupati Rao <trao@saisoftek.net>
2023-12-05 14:07:39 +0530

    TFS-9509

--- src/_components/scheduleInterview/upcomingVideoDetails.js
+++ src/_components/scheduleInterview/upcomingVideoDetails.js
@@ -247,12 +247,13 @@ export function UpcomingVideoDetails({
             ? "Rejected"
             : "No response"}
         </div>
-        {interviewDetails?.interviewstatusid !== 0 && (
-          <div className="p-custom">
-            <h6 className="fw-bold mb-0 job-heading">Interview feedback</h6>
-            {interviewDetails?.interviewfeedback}
-          </div>
-        )}
+        {interviewDetails?.interviewstatusid !== 0 &&
+          interviewDetails?.interviewfeedback !== "" && (
+            <div className="p-custom">
+              <h6 className="fw-bold mb-0 job-heading">Interview feedback</h6>
+              {interviewDetails?.interviewfeedback}
+            </div>
+          )}
         {showInviteCard === true && (
           <div className="mt-2 mb-2">
             <InviteToInterviewCard
