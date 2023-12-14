cd7e57682a84a5cbdad571edc50af4b41c91e52f
Tirupati Rao <trao@saisoftek.net>
2023-12-14 17:52:08 +0530

    TFS-9644

--- src/_containers/customer/newjobs/custjobdetails.js
+++ src/_containers/customer/newjobs/custjobdetails.js
@@ -606,7 +606,7 @@ export function CustJobDetail({
             {!isModal ? (
               <CardFooter>
                 Share:{" "}
-                {/* <ShareSocial
+                <ShareSocial
                   url={
                     window.location.origin +
                     "/job-detail/" +
@@ -624,7 +624,7 @@ export function CustJobDetail({
                       padding: "0px",
                     },
                   }}
-                /> */}
+                />
               </CardFooter>
             ) : (
               <></>
