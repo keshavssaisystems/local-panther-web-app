95d8451969f2ebaefe8358ac944e4344d02ad1db
Saisystemspramod <pramod.turakane@businesswire.com>
2024-03-05 16:05:28 +0530

    schedule interview button changes and candidate dashboard icon changes

--- src/assets/utils/images/customer/dashboard/index.js
+++ src/assets/utils/images/customer/dashboard/index.js
@@ -5,6 +5,16 @@ const custDashIcons = {
   offer: require("./offer.svg").default,
   open: require("./open.svg").default,
   pending: require("./pending.svg").default,
+  activecandidates: require("./activecandidates.svg").default,
+  openjobs: require("./openjobs.svg").default,
+  activeclient: require("./activeclient.svg").default,
+  activeemployees: require("./activeemployees.svg").default,
+  candAccepted: require("./candAccepted.svg").default,
+  candApplied: require("./candApplied.svg").default,
+  candInterview: require("./candInterview.svg").default,
+  candMatched: require("./candMatched.svg").default,
+  candOffer: require("./candOffer.svg").default,
+  candRejected: require("./candRejected.svg").default,
 };
 
 export default custDashIcons;
