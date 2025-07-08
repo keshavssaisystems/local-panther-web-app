25a95018f73c4d4a278db89cc8f9aa1e323ece10
Pramod Turakane <pturakane@sst.saisystems.com>
2025-07-08 14:46:33 +0530

    Added changes for download interview questions

--- src/_components/scheduleInterview/upcomingVideoDetails.js
+++ src/_components/scheduleInterview/upcomingVideoDetails.js
@@ -13,10 +13,16 @@ import {
   ModalHeader,
   ModalBody,
   Modal,
+  Row,
 } from "reactstrap";
 import "./scheduledInterview.scss";
 import { FaEllipsisV } from "react-icons/fa";
-import { BsPersonVideo2, BsTelephone, BsPerson } from "react-icons/bs";
+import {
+  BsPersonVideo2,
+  BsTelephone,
+  BsPerson,
+  BsDownload,
+} from "react-icons/bs";
 import { ImBin } from "react-icons/im";
 import moment from "moment-timezone";
 import { useSelector } from "react-redux";
@@ -29,6 +35,7 @@ import { getTimezoneDateTime, getVideoChannelId } from "_helpers/helper";
 import { NavLink } from "react-router-dom";
 import { InterviewFeedback } from "./interviewFeedback";
 import { USPhoneNumber } from "_helpers/helper";
+import html2pdf from "html2pdf.js";
 export function UpcomingVideoDetails({
   interviewId,
   cancelScheduleData,
@@ -173,6 +180,25 @@ export function UpcomingVideoDetails({
     suggestedJson = "";
     suggestedQuestionArray = [];
   }
+
+  const generatePDF = async (event) => {
+    const element = document.getElementById("sq-pdf-content");
+
+    if (element) {
+      const pdfOptions = {
+        margin: 10,
+        html2canvas: {
+          scale: 1.2,
+          useCORS: true,
+        },
+        filename: interviewDetails.candidatename + " inetrviewquestions",
+        image: { type: "jpeg", quality: 0.98 },
+        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+      };
+
+      html2pdf().from(element).set(pdfOptions).save();
+    }
+  };
 
   return (
     <>
@@ -519,13 +545,36 @@ export function UpcomingVideoDetails({
         </div>
         {interviewDetails?.suggestedquestion !== "" && (
           <div className="p-3 suggested-question">
-            <h6 className="fw-bold">Suggested Questions</h6>
-            {suggestedQuestionArray?.length > 0 &&
-              suggestedQuestionArray?.map((suggestedQuestion) => (
-                <>
-                  <p className="mb-1">{suggestedQuestion}</p>
-                </>
-              ))}
+            <Row>
+              <Col md="11">
+                <h6 className="fw-bold">Suggested Questions</h6>
+              </Col>
+              {suggestedQuestionArray?.length > 0 && (
+                <Col md="1">
+                  <div
+                    style={{ cursor: "pointer" }}
+                    title="Click here to download suggested questions"
+                    onClick={() => generatePDF()}
+                  >
+                    <h6>
+                      {" "}
+                      <BsDownload />
+                    </h6>
+                  </div>
+                </Col>
+              )}
+            </Row>
+            <div id="sq-pdf-content">
+              {suggestedQuestionArray?.length > 0 && (
+                <ol type="1">
+                  {suggestedQuestionArray?.map((suggestedQuestion) => (
+                    <>
+                      <li className="mb-1 ">{suggestedQuestion}</li>
+                    </>
+                  ))}
+                </ol>
+              )}
+            </div>
             {suggestedQuestionArray?.length === 0 && (
               <p className="mb-0 ">
                 <i> - No suggested question added</i>
