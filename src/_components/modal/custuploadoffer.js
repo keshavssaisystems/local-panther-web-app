8712b3e79a8b7047c74642d131f24e5b1031f6b0
kumars2021 <kumars@tech.saisystems.com>
2025-09-01 14:57:51 +0530

    bugid- 13301 & 13305- offer related issues

--- src/_components/modal/custuploadoffer.js
+++ src/_components/modal/custuploadoffer.js
@@ -302,43 +302,44 @@ export const CustomerUploadOffer = (props) => {
     >
       <ModalHeader toggle={() => props.onClose()}>Make Offer</ModalHeader>
       <ModalBody
-        style={{ maxHeight: "75vh", overflow: "auto", minHeight: "40vh" }}
+        style={{ maxHeight: "75vh", overflow: "auto", minHeight: "65vh" }}
       >
-        {props.loading ? (
-          <div className="offer-loading-div">
-            <Loader
-              type="line-scale-pulse-out-rapid"
-              className="d-flex justify-content-center"
-            />
-          </div>
-        ) : (
-          <div>
-            <Nav fill pills>
-              <NavItem>
-                <NavLink
-                  active={activeTab === 2}
-                  // className={activeTab === 2 ? "active" : ""}
-                  onClick={() => {
-                    setActiveTab(2);
-                  }}
-                >
-                  System Generated Offer
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  active={activeTab === 1}
-                  // className={activeTab === 1 ? "active" : ""}
-                  onClick={() => {
-                    setActiveTab(1);
-                  }}
-                >
-                  Manual Upload Offer
-                </NavLink>
-              </NavItem>
-              <TabContent activeTab={activeTab}>
-                <hr style={{ margin: "0px", marginBottom: "1rem" }}></hr>
-                <TabPane tabId={1}>
+        <div>
+          <Nav fill pills>
+            <NavItem>
+              <NavLink
+                active={activeTab === 2}
+                // className={activeTab === 2 ? "active" : ""}
+                onClick={() => {
+                  setActiveTab(2);
+                }}
+              >
+                System Generated Offer
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                active={activeTab === 1}
+                // className={activeTab === 1 ? "active" : ""}
+                onClick={() => {
+                  setActiveTab(1);
+                }}
+              >
+                Manual Upload Offer
+              </NavLink>
+            </NavItem>
+            <TabContent activeTab={activeTab}>
+              <hr style={{ margin: "0px", marginBottom: "1rem" }}></hr>
+              <TabPane tabId={1}>
+                <div className="tab-pane-wrapper">
+                  {props.loading && (
+                    <div className="overlay-loader">
+                      <Loader
+                        type="line-scale-pulse-out-rapid"
+                        className="d-flex justify-content-center"
+                      />
+                    </div>
+                  )}
                   <Row>
                     {" "}
                     <Col xs={12} sm={12} md={12} lg={4} xl={4} xxl={4}>
@@ -466,8 +467,18 @@ export const CustomerUploadOffer = (props) => {
                       <Label className="ps-1"> Is final offer</Label>
                     </Col>
                   </Row>
-                </TabPane>
-                <TabPane tabId={2}>
+                </div>
+              </TabPane>
+              <TabPane tabId={2}>
+                <div className="tab-pane-wrapper">
+                  {props.loading && (
+                    <div className="overlay-loader">
+                      <Loader
+                        type="line-scale-pulse-out-rapid"
+                        className="d-flex justify-content-center"
+                      />
+                    </div>
+                  )}
                   <Row>
                     {" "}
                     {!showPdfPrev && (
@@ -635,11 +646,13 @@ export const CustomerUploadOffer = (props) => {
                       </>
                     )}
                   </Row>
-                </TabPane>
-              </TabContent>
-            </Nav>
-          </div>
-        )}
+
+                </div>
+              </TabPane>
+            </TabContent>
+          </Nav>
+        </div>
+
       </ModalBody>
       <ModalFooter>
         <ButtonGroup>
@@ -660,8 +673,8 @@ export const CustomerUploadOffer = (props) => {
             {activeTab === 1
               ? "Upload File"
               : showPdfPrev && activeTab === 2
-              ? "Confirm & Submit"
-              : "Generate offer"}
+                ? "Confirm & Submit"
+                : "Generate offer"}
           </Button>
           <Button color="secondary" onClick={() => props.onClose()}>
             Close
