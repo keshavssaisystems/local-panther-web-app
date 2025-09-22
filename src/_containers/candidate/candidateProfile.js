789f68584fbd85c68be0bd5a3180a2c9d7239b51
kumars2021 <kumars@tech.saisystems.com>
2025-09-22 21:22:26 +0530

    enhancement suggested by QA

--- src/_containers/candidate/candidateProfile.js
+++ src/_containers/candidate/candidateProfile.js
@@ -14,6 +14,7 @@ import { CandidateSkills } from "./candidateSkills";
 import { CertificationDetails } from "./certifications";
 import { AdditionalInformation } from "./additionalInfo";
 import { JobPreferences } from "./jobPreferences";
+import OpenWroXAgent from '../../assets/utils/images/OpenWroXAgent.svg'
 import {
   dropdownActions,
   getSkillsFilter,
@@ -359,8 +360,14 @@ export function CandidateProfile() {
 
         <div className="col-7 profile-summary">
           <div className="float-end">
-            <Button type="button" className="me-2 save-btn btn btn-secondary" onClick={() => handleAIProfile()}>Update Profile with OpenWorx Agent</Button>
-            {/* <AIProfileOffCanvas></AIProfileOffCanvas> */}
+
+            <Button type="button" className="me-2 save-btn btn btn-sm btn-secondary" onClick={() => handleAIProfile()}>
+              <img
+                src={OpenWroXAgent}
+                alt="Update profile"
+                className={"icon-pointer me-2"}
+              ></img>
+              Update Profile with OpenWorX Agent</Button>
           </div>
         </div>
       </div>
