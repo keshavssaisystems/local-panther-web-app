8d3d5339510b8129d7fcddc9911fa5da0b308da8
kumars2021 <kumars@tech.saisystems.com>
2025-12-04 17:57:16 +0530

    assignd to on Job

--- src/_components/createJobComponents/publishJobStep.js
+++ src/_components/createJobComponents/publishJobStep.js
@@ -48,7 +48,7 @@ export default function PublishJobStep({
       securityclearanceid: reqData.basicInformation.securityclearance,
       hiringmanagerid: String(reqData.basicInformation?.hiringmanagerid),
       clientcompanyid: reqData.basicInformation?.clientcompanyid,
-      assignedtoid: reqData.basicInformation?.assignedtoid,
+      recruiterid: String(reqData.basicInformation?.recruiterid),
       jobExperienceScheduleDtos: [
         {
           jobexperiencescheduleid: 0,
