7b08783f2832a7df0c50d77358a44c5f8d042bcf
kumars2021 <kumars@tech.saisystems.com>
2026-02-26 21:35:41 +0530

    Added On-Assignment icon

--- src/_components/list/cardview.js
+++ src/_components/list/cardview.js
@@ -537,8 +537,8 @@ export const CandidateCardView = (props) => {
                           <img
                             src={customerIcons.On_Assignment}
                             alt="On assignment"
-                            width="22"
-                            height="25"
+                            width="16"
+                            height="auto"
                             style={{ verticalAlign: "middle", objectFit: "contain" }}
                           />
                         </span>
