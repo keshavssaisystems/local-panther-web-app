8e32a8cd2fcd6dff09dbea22956b9d51c148867a
skudale <skudale@SSTL-048>
2026-05-20 16:17:36 +0530

    updated the columns headers

--- src/_containers/common/FeedBack/feedBack.js
+++ src/_containers/common/FeedBack/feedBack.js
@@ -123,12 +123,12 @@ const FeedBack = () => {
   if (userroleid === "1") {
     columns.splice(0, 0,
       {
-        name: "Submitter Name",
+        name: "Name",
         selector: (row) => row.submittername || "",
         cell: (row) => <span title={row.submittername}>{row.submittername || "-"}</span>,
       },
       {
-        name: "Submitter Email",
+        name: "Email",
         selector: (row) => row.submitteremail || "",
         cell: (row) => <span title={row.submitteremail}>{row.submitteremail || "-"}</span>,
       }
